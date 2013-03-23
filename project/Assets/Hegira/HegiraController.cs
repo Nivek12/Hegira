c98a4f61bc217d20cc009912c7ffe8cd69bd4c5c
Nivek12 <klcme33@gmail.com>
2013-03-22 23:22:03 -0400

    -Added models for the outpost and hegira. Also added a minimap

---
+++ project/Assets/Hegira/HegiraController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class HegiraController : MonoBehaviour {
+	
+	bool showLaunch;
+	
+	// Use this for initialization
+	void Start () {
+		showLaunch = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		
+		//If the player collided with the trigger then show them the launch button
+		if(other.gameObject.GetComponent("PlayerController") != null) {
+			showLaunch = true;
+		}
+		
+	}
+	
+	void OnTriggerExit(Collider other) {
+		
+		//If the player exits the then hide the launch button
+		if(other.gameObject.GetComponent("PlayerController") != null) {
+			showLaunch = false;
+		}
+		
+	}
+	
+	void OnGUI() {
+		
+		if(showLaunch) {
+			
+			if(GUI.Button(new Rect(500 , 250, 500, 100), "SHIT SHIT GTFO" )) {
+				
+			}
+			
+		}
+		
+	}
+}
